dd82b1dc9a8833ca22bd85c15f9427ed03641d92
oladayosolomon <oladayosolomon@gmail.com>
2025-12-22 10:45:19 +0900

    Refactor Hopper class documentation and comments
    Updated comments and formatting for clarity.

--- Problems/Single-objective optimization/RL/Hopper.m
+++ Problems/Single-objective optimization/RL/Hopper.m
@@ -1,18 +1,6 @@
 classdef Hopper < PROBLEM
 % <single> <real> <large/none>
-% Benchmark MOP with bias feature
-
-%------------------------------- Reference --------------------------------
-% H. Li, Q. Zhang, and J. Deng, Biased multiobjective optimization and
-% decomposition algorithm, IEEE Transactions on Cybernetics, 2017, 47(1):
-% 52-66.
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2023 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+% DRL Hooper Task
 %--------------------------------------------------------------------------
 
     methods
@@ -33,22 +21,7 @@ classdef Hopper < PROBLEM
             PopObj = double(PopObj);
         end
         function Population = Evaluation(obj,varargin)
-        %Evaluation - Evaluate multiple solutions.
-        %
-        %   P = obj.Evaluation(Dec) returns the SOLUTION objects based on
-        %   the decision variables Dec. The objective values and constraint
-        %   violations of the solutions are calculated automatically, and
-        %   obj.FE is increased accordingly.
-        %
-        %   P = obj.Evaluation(Dec,Add) also sets the additional properties
-        %   (e.g., velocity) of solutions.
-        %
-        %   This function is usually called after generating new solutions.
-        %
-        %   Example:
-        %       Population = Problem.Evaluation(PopDec)
-        %       Population = Problem.Evaluation(PopDec,PopVel)
-            
+                  
             PopDec     = obj.CalDec(varargin{1});
             PopObj     = obj.CalObj(PopDec);
             PopCon     = obj.CalCon(PopDec);
@@ -57,3 +30,4 @@ classdef Hopper < PROBLEM
         end
     end
 end 
+
