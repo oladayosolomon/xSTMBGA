f4146eab811244539263b1958b0aed33b3b70d8a
oladayosolomon <oladayosolomon@gmail.com>
2025-12-22 10:43:16 +0900

    Update FrozenLake.m

--- Problems/Single-objective optimization/RL/FrozenLake.m
+++ Problems/Single-objective optimization/RL/FrozenLake.m
@@ -1,18 +1,7 @@
 classdef FrozenLake < PROBLEM
 % <single> <real> <large/none>
-% Benchmark MOP with bias feature
+% DRL FrozenLake Task
 
-%------------------------------- Reference --------------------------------
-% H. Li, Q. Zhang, and J. Deng, Biased multiobjective optimization and
-% decomposition algorithm, IEEE Transactions on Cybernetics, 2017, 47(1):
-% 52-66.
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2023 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
 %--------------------------------------------------------------------------
 
     methods
@@ -57,3 +46,4 @@ classdef FrozenLake < PROBLEM
         end
     end
 end 
+
